17123dd87131347ff7428364259eadebd23c3d54
AlonaShapoval99 <yagjJW+774:ne^9>
2021-11-02 19:56:44 +0200

    Initial commit

---
+++ src/main/java/com/shapoval/lab2_greedy_algorithm/Lab2GreedyAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.shapoval.lab2_greedy_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2GreedyAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2GreedyAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shapoval/lab2_greedy_algorithm/Lab2GreedyAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shapoval.lab2_greedy_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2GreedyAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
